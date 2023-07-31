bf305251b77b9058c7e00ee363bf98fdfd796bf1
Sávio Pessôa Afonso <115905335+savioomio@users.noreply.github.com>
2023-07-31 11:21:34 -0300

    Add files via upload

--- pages/perfil.php
+++ pages/perfil.php
@@ -1,10 +1,44 @@
 <?php
-    session_start();
-    require_once('../metodos/sis_cadastro_login/val_sessao.php');
-    validar_sessao('login.php');
+session_start();
+require_once('../metodos/sis_cadastro_login/val_sessao.php');
+validar_sessao('login.php');
+require_once "../metodos/sis_cadastro_login/conn.php";
+$sessionUsername = $_SESSION["userLogin"];
+
+// Step 3: Use prepared statements para buscar os dados do usuário
+$query = "SELECT * FROM usuario WHERE username = :username";
+$stmt = $conn->prepare($query);
+$stmt->bindParam(":username", $sessionUsername, PDO::PARAM_STR);
+$stmt->execute();
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (isset($_FILES["fileImg"]["name"])) {
+    $username = $_POST["username"];
+
+    // Step 6: Renomeie os arquivos de upload de forma mais segura
+    $originalFileName = $_FILES["fileImg"]["name"];
+    $randomValue = uniqid(mt_rand(), true); // Valor aleatório baseado no tempo atual
+    $extension = pathinfo($originalFileName, PATHINFO_EXTENSION); // Obter a extensão do arquivo original
+
+    // Criar um nome único usando sha1 (outras funções hash também podem ser usadas)
+    $imageName = sha1($originalFileName . $randomValue) . '.' . $extension;
+
+    $target = "img_perfil/" . $imageName;
+    move_uploaded_file($_FILES["fileImg"]["tmp_name"], $target);
+
+    $query = "UPDATE usuario SET img_perfil = :img_perfil WHERE username = :username";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(":img_perfil", $imageName, PDO::PARAM_STR);
+    $stmt->bindParam(":username", $username, PDO::PARAM_STR);
+    $stmt->execute();
+
+    header("Refresh: 0.1;");
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,7 +47,9 @@
     <link rel="shortcut icon" href="../img/logo_jsor.png" type="image/x-icon">
     <link rel="stylesheet" href="./style/perfil.css">
     <script type="module" src="../javascript/dark_nuvem_lista.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
     <main>
         <section id="logo">
@@ -37,25 +73,39 @@
                     <span class="star star--6"></span>
                 </label>
             </div>
-            <!-- <input type="checkbox" name="" id="switch"> -->
         </section>
         <section id="area_editavel">
             <section id="img_perfil">
                 <div id="foto_perfil">
+                    <form class="form" id="form" action="" enctype="multipart/form-data" method="post">
+                        <input type="hidden" name="username" value="<?php echo $user['username']; ?>">
+                        <div class="upload">
+                            <img src="img_perfil/<?php echo $user['img_perfil']; ?>" id="image">
 
+                            <div class="rightRound" id="upload">
+                                <input type="file" name="fileImg" id="fileImg" accept=".jpg, .jpeg, .png">
+                                <i class="fa fa-camera"></i>
+                            </div>
+
+                            <div class="leftRound" id="cancel" style="display: none;">
+                                <i class="fa fa-times"></i>
+                            </div>
+                            <div class="rightRound" id="confirm" style="display: none;">
+                                <input type="submit">
+                                <i class="fa fa-check"></i>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </section>
             <section id="dados_perfil">
                 <div id="info_usuario">
-                <?php
+                    <?php
                     require_once "../metodos/sis_busca_amizade/functions.php";
-                    $id = isset($_GET['id']) ? @$_GET['id'] : $_SESSION['id'];
+                    $id = isset($_GET['id']) ? $_GET['id'] : $_SESSION['id'];
                     get_perfil($conn, $id);
-                ?>
+                    ?>
                 </div>
-                <!--<div id="botao_editar_perfil">
-                    <button type="submit" id="editar_perfil">Editar Perfil</button>
-                </div>-->
             </section>
         </section>
         <section id="area_desempenho">
@@ -70,8 +120,8 @@
         </section>
         <section class="navigation">
             <ul>
-            <li class="list active">
-                    <a href="<?=$_SERVER['PHP_SELF']?>">
+                <li class="list active">
+                    <a href="<?= $_SERVER['PHP_SELF'] ?>">
                         <span class="icon">
                             <ion-icon name="person-circle-sharp"></ion-icon>
                         </span>
@@ -120,7 +170,7 @@
                     </a>
                 </li>
 
-                <div class="indicator"></div> 
+                <div class="indicator"></div>
             </ul>
 
         </section>
@@ -128,6 +178,29 @@
     </main>
     <script src='https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js'></script>
     <script src='https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js'></script>
-</body>
-</html>
 
+    <script type="text/javascript">
+      document.getElementById("fileImg").onchange = function(){
+        document.getElementById("image").src = URL.createObjectURL(fileImg.files[0]); // Preview new image
+
+        document.getElementById("cancel").style.display = "block";
+        document.getElementById("confirm").style.display = "block";
+
+        document.getElementById("upload").style.display = "none";
+      }
+
+      var userImage = document.getElementById('image').src;
+      document.getElementById("cancel").onclick = function(){
+        document.getElementById("image").src = userImage; // Back to previous image
+
+        document.getElementById("cancel").style.display = "none";
+        document.getElementById("confirm").style.display = "none";
+
+        document.getElementById("upload").style.display = "block";
+      }
+    </script>
+
+
+</body>
+
+</html>
